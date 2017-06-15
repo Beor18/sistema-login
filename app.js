f312bede0e67b25ddaf07031d50226e94c0e6364
Fernando Lopez <fer.ragnar19@gmail.com>
2017-06-14 21:57:12 -0300

    actualizado app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const mysql = require("mysql")
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -28,11 +29,19 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//Conexion con MySql
+con = mysql.createConnection({
+  host:"localhost",
+  user:"root",
+  password:"root",
+  database:"nodeapp"
+});
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -63,6 +72,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 app.listen(port, function(err) {
 	if(err)
