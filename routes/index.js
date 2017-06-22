7e92887b1445ad33098571908909016136eccd53
Fernando Lopez <fer.ragnar19@gmail.com>
2017-06-22 00:34:14 -0300

    agregado carga de imagen no terminado

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,52 @@ var express = require('express');
 var passport = require('passport');
 var router = express.Router();
 
+var fs = require("fs");
+
+var multer = require("multer");
+var upload = multer({dest: "./uploads"});
+
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/images");
+var conn = mongoose.connection;
+
+var gfs;
+
+
+var Grid = require("gridfs-stream");
+Grid.mongo = mongoose.mongo;
+
+conn.once("open", function(){
+  gfs = Grid(conn.db);
+  router.get("/sultan", function(req,res){
+    //renders a multipart/form-data form
+    res.render("sultan");
+  });
+
+  //segundo parametro es multer.
+  router.post("/sultan", upload.single("avatar"), function(req, res, next){
+    var writestream = gfs.createWriteStream({
+      filename: req.file.originalname
+    });
+    //
+    fs.createReadStream("./uploads/" + req.file.filename)
+      .on("end", function(){fs.unlink("./uploads/"+ req.file.filename, function(err){res.send("success")})})
+        .on("err", function(){res.send("Error al subir imagen")})
+          .pipe(writestream);
+  });
+
+  // envio imagen por filename.
+  router.get("/:filename", function(req, res){
+      var readstream = gfs.createReadStream({filename: req.params.filename});
+      readstream.on("error", function(err){
+        res.send("No image found with that title");
+      });
+      readstream.pipe(res);
+  });
+
+});
+
 router.get('/', (req, res, next) => {
 	res.render('index', {title: 'Express'});
 });
